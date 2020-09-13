82e079197f155dca467fdb00486c53a3d8150020
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-13 20:57:53 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
   # code goes here
+  attr_accessor :name,:owner
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end

6ccaa8191d03931873ba7600cec24b4dfdd54bd1
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-13 21:06:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,9 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
-  def initiale(name)
+  def initiale(name, owner)
     @name = name
-
+    @owner = owner
+    @mood = "nervous"
   end
 end

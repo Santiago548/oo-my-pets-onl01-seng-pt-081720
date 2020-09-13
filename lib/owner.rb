ee68884fad8f43e2b3dc1943d39c8977bb1c0bcd
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-13 21:02:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,28 +1,4 @@
 class Owner
   # code goes here
-  attr_reader :name, :species
 
-  @@all =[]
-
-  def initialize(name, species)
-    @name = name
-    @species = species
-    @@all << self
-  end
-
-  def say_species
-
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.count
-    @@all.count
-  end
-
-  def self.reset_all
-    owner.reset
-  end
 end
